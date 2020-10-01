a854795a972ce0e38e8b0a1b1730f1964bd78e6b
YASH <72142451+yashcoder-1@users.noreply.github.com>
2020-10-01 11:16:55 +0530

    Update Insertion sort.C

--- Insertion sort.C
+++ Insertion sort.C
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int main(){
+    
+      /* Here i & j for loop counters, key for swapping,
+    * count for total number of elements, number[] to
+    * store the input numbers in array. You can increase
+    * or decrease the size of number array as per requirement
+    */
+    
     int N,key,i,j;
     scanf("%d",&N);
     int arr[N];
     for(i=0;i<N;i++){
         scanf("%d",&arr[i]);
     }
+    // Implementation of insertion sort algorithm
     for(j=1;j<N;j++){
         key=arr[j];
         i=j-1;
